7b611e92176ce62d3eaf7bb321399af310a21114
Bars <bars303a@gmail.com>
2026-03-19 20:56:29 +0500

    Preparation for communication #1

--- source/client.c
+++ source/client.c
@@ -6,56 +6,55 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 
+#define BUFFER_SIZE 1024
+
 int main() {
-	int sfd, opt;
-	struct sockaddr_in addr;
-	socklen_t size;
-	char buf[1024];
-	size_t buf_size;
+  int sfd, opt;
+  struct sockaddr_in addr;
+  socklen_t size;
+  char buf[BUFFER_SIZE];
+  ssize_t buf_size;
 
-	sfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sfd == -1) {
-		/* cannot create socket */
-		exit(EXIT_FAILURE);
-	}
-	opt = 1;
-	setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+  sfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sfd == -1) {
+    perror("create socket error\n");
+    exit(EXIT_FAILURE);
+  }
+  opt = 1;
+  setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-	addr.sin_family = AF_INET;
+  addr.sin_family = AF_INET;
+  /* local address 127.0.0.1 */
+  addr.sin_addr.s_addr = htonl(0x7F000001);
+  /* server port */
+  addr.sin_port = htons(9997);
 
-	/* local address 127.0.0.1 */
-	addr.sin_addr.s_addr = htonl(0x7F000001);
-	/* server port */
-	addr.sin_port = htons(9997);
+  size = sizeof(addr);
 
-	size = sizeof(addr);
+  if(connect(sfd, (struct sockaddr*) &addr, size) == -1) {
+    perror("connection error\n");
+    exit(EXIT_FAILURE);
+  }
 
-	if(connect(sfd, (struct sockaddr*) &addr, size) == -1) {
-		printf("error\n");
-		/* connection error */
-		exit(EXIT_FAILURE);
-	}
+  printf("Enter your username\n");
+  if(fgets(buf, BUFFER_SIZE, stdin) == NULL) {
+    perror("read from stdin error\n");
+    exit(EXIT_FAILURE);
+  }
+  buf_size = write(sfd, buf, BUFFER_SIZE);
+  if(buf_size == -1) {
+    perror("error writing to the server\n");
+    exit(EXIT_FAILURE);
+  }
 
-	printf("You can send 3 messages\n");
-	for(int i = 0; i < 3; i++) {
-		buf_size = read(STDIN_FILENO, buf, 1024);
-		buf[buf_size - 1] = '\0';
+  buf_size = read(sfd, buf, BUFFER_SIZE);
+  if(buf_size == -1) {
+    perror("error reading from the server\n");
+    exit(EXIT_FAILURE);
+  }
+  buf[buf_size] = 0;
+  printf("Server response: %s\n", buf);
 
-		if(buf_size == -1) {
-			exit(EXIT_FAILURE);
-		}
-		else if(buf_size == 0) {
-			break;
-		}
-		else {
-			if(write(sfd, buf, buf_size) == -1) {
-				exit(EXIT_FAILURE);
-			}
-			buf_size = read(sfd, buf, buf_size);
-			printf("Server response: %s\n", buf);
-		}
-	}
-
-	close(sfd);
-	return 0;
+  close(sfd);
+  return 0;
 }
--- source/main.c
+++ source/main.c
@@ -14,160 +14,226 @@
 
 #include <fcntl.h>
 #include <string.h>
+#include <errno.h>
+#include "dynamic_array.h"
 
 #define MAX_EVENTS 10
+#define BUFFER_SIZE 1024
 
-struct clients {
-	int client_fds[MAX_EVENTS - 1];
-	size_t size;
-	char *username;
+typedef enum client_states ClientStates;
+typedef struct client Client;
+
+enum client_states {
+  WAITING_USERNAME,
 };
-typedef struct clients Clients;
+
+struct _server {
+  int listen_socket;
+  int epoll_socket;
+  Darray *clients;
+};
+
+struct client {
+  int fd; 
+  char *username;
+  ClientStates state;
+};
+
 
 int create_listening_socket(unsigned short port);
-void accept_conn_req(int efd, int listen_sock, Clients *clients);
+void accept_conn_req(struct _server server);
 void set_nonblocking(int ifd);
-void process_event(int epoll_fd, struct epoll_event event);
+void process_event(struct _server server, struct epoll_event event);
+Client create_client(int fd);
+struct _server server_init(unsigned short port);
+size_t find_fd_index(Darray *a, int fd);
+
+struct _server server_init(unsigned short port) 
+{
+  struct _server result;
+  int epoll_socket, listen_socket;
+  struct epoll_event ev;
+
+  listen_socket = create_listening_socket(port);
+  epoll_socket = epoll_create1(0);
+  if(epoll_socket == -1) {
+    perror("epoll create error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  ev.events = EPOLLIN;
+  ev.data.fd = listen_socket;
+  if(epoll_ctl(epoll_socket, EPOLL_CTL_ADD, listen_socket, &ev) == -1) {
+    perror("can't add listen socket to epoll\n");
+    exit(EXIT_FAILURE);
+  }
+
+  result.clients = darray_new(0, sizeof(Client));
+  result.listen_socket = listen_socket;
+  result.epoll_socket = epoll_socket;
+  return result;
+}
+
+Client create_client(int fd)
+{
+  Client result;
+  result.fd = fd;
+  result.state = WAITING_USERNAME;
+  return result;
+}
 
 int main() 
 {
-	Clients clients = { .size = 0 };
-	int listen_sock, epoll_fd, nfds;
-	struct epoll_event ev;
-	struct epoll_event events[MAX_EVENTS];
+  struct _server server;
+  int nfds;
+  struct epoll_event events[MAX_EVENTS];
 
-	epoll_fd = epoll_create1(0);
-	if(epoll_fd == -1) {
-		/* epoll create error */
-		exit(EXIT_FAILURE);
-	}
+  Client *tmp;
 
-	listen_sock = create_listening_socket(9997);
-	printf("listen socket %d\n", listen_sock);
+  server = server_init(9997);
+  printf("listen socket %d\n", server.listen_socket);
 
-	ev.events = EPOLLIN;
-	ev.data.fd = listen_sock;
-	if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_sock, &ev) == -1) {
-		/* epoll ctl error */
-		exit(EXIT_FAILURE);
-	}
-	for(;;) {
-		nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
-		if(nfds == -1) {
-			/* epoll_wait error */
-			exit(EXIT_FAILURE);
-		}
-		printf("%d\n", nfds);
-		for(int n = 0; n < nfds; n++) {
-			printf("events %u\n", events[n].events);
-			if(events[n].data.fd == listen_sock) {
-				/* accept connection */
-				accept_conn_req(epoll_fd, listen_sock, &clients);
-			}
-			else {
-				process_event(epoll_fd, events[n]);
-			}
-		}
-	}
+  for(;;) {
+    nfds = epoll_wait(server.epoll_socket, events, MAX_EVENTS, -1);
+    if(nfds == -1) {
+      /* epoll_wait error */
+      exit(EXIT_FAILURE);
+    }
 
-	/* clear all file descriptors from {events} */
-	close(epoll_fd);
-	close(listen_sock);
-	printf("program finished successfully\n");
+    printf("List of client file descriptors: ");
+    for(size_t i = 0; i < server.clients->size; i++) {
+      tmp = darray_get(server.clients, i);
+      printf("%d, ", tmp->fd);
+    }
+    printf("\n");
+    for(int n = 0; n < nfds; n++) {
+      if(events[n].data.fd == server.listen_socket) {
+        /* accept connection */
+        accept_conn_req(server);
+      }
+      else {
+        process_event(server, events[n]);
+      }
+    }
+  }
 
-	return 0;
+  /* clear all file descriptors from {events} */
+  /*close(epoll_fd);
+  close(listen_sock);*/
+  printf("program finished successfully\n");
+
+  return 0;
 }
 
 
 
-int create_listening_socket(unsigned short port)
-{
-	int sock_fd, opt;
-	unsigned int queue_size = 5;
-	struct sockaddr_in addr;
-	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sock_fd == -1) {
-		/*cannot create socket */
-		exit(EXIT_FAILURE);
-	}
+int create_listening_socket(unsigned short port) {
+  int sock_fd, opt;
+  unsigned int queue_size = 5;
+  struct sockaddr_in addr;
+  sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sock_fd == -1) {
+    perror("can't create a listening socket\n");
+    /*cannot create socket */
+    exit(EXIT_FAILURE);
+  }
 
-	opt = 1;
-	setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(port);
-	if(bind(sock_fd, (struct sockaddr*) &addr, sizeof(addr)) == -1) {
-		/* cannot bind socket */
-		exit(EXIT_FAILURE);
-	}
-	if(listen(sock_fd, queue_size) == -1) {
-		/* cannot enable listening mode */
-		exit(EXIT_FAILURE);
-	}
-	return sock_fd;
+  opt = 1;
+  setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = htons(port);
+  if(bind(sock_fd, (struct sockaddr*) &addr, sizeof(addr)) == -1) {
+    perror("listening socket binding error\n");
+    exit(EXIT_FAILURE);
+  }
+  if(listen(sock_fd, queue_size) == -1) {
+    perror("can't enable listening mode\n");
+    exit(EXIT_FAILURE);
+  }
+  return sock_fd;
 }
 
-void accept_conn_req(int efd, int listen_sock, Clients *clients) {
-	int new_fd;
-	struct sockaddr_in addr;
-	struct epoll_event ev;
-	socklen_t  addr_len;
-	
-	addr_len = sizeof(addr);
-	new_fd= accept(listen_sock, (struct sockaddr*) &addr, &addr_len);
-	if(new_fd == -1) {
-		/* cannot accept connection */
-		exit(EXIT_FAILURE);
-	}
-	set_nonblocking(new_fd);
-	printf("client address %hhu.%hhu.%hhu.%hhu:%hu\n",
-			addr.sin_addr.s_addr,
-			addr.sin_addr.s_addr >> 8,
-			addr.sin_addr.s_addr >> 16,
-			addr.sin_addr.s_addr >> 24,
-			ntohs(addr.sin_port)
-			);
+void accept_conn_req(struct _server server) {
+  int new_fd;
+  Client new_client;
+  struct sockaddr_in addr;
+  struct epoll_event ev;
+  socklen_t  addr_len;
 
-	/* add socket to tracked by epoll */
-	ev.events = EPOLLIN | EPOLLET;
-	ev.data.fd = new_fd;
-	if(epoll_ctl(efd, EPOLL_CTL_ADD, new_fd, &ev) == -1) {
-		/* epoll ctl error */
-		exit(EXIT_FAILURE);
-	}
-	clients->client_fds[clients->size++] = new_fd;
+  addr_len = sizeof(addr);
+  new_fd= accept(server.listen_socket, (struct sockaddr*) &addr, &addr_len);
+  if(new_fd == -1) {
+    perror("accept connection error\n");
+    exit(EXIT_FAILURE);
+  }
+  set_nonblocking(new_fd);
+  printf("new client address %hhu.%hhu.%hhu.%hhu:%hu\n",
+    addr.sin_addr.s_addr,
+    addr.sin_addr.s_addr >> 8,
+    addr.sin_addr.s_addr >> 16,
+    addr.sin_addr.s_addr >> 24,
+    ntohs(addr.sin_port)
+  );
+
+  /* add socket to tracked by epoll */
+  ev.events = EPOLLIN | EPOLLET;
+  ev.data.fd = new_fd;
+  if(epoll_ctl(server.epoll_socket, EPOLL_CTL_ADD, new_fd, &ev) == -1) {
+    /* epoll ctl error */
+    exit(EXIT_FAILURE);
+  }
+  new_client = create_client(new_fd);
+  if(darray_push(server.clients, &new_client) != DA_OK) {
+    perror("accept connection: darray push error\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+size_t find_fd_index(Darray *a, int fd) {
+  Client *client;
+  size_t index;
+  for(index = 0; index < a->size; index++) {
+    client = darray_get(a, index);
+    if(client != NULL && client->fd == fd) {
+      break;
+    }
+  }
+  return index;
 }
 
 void set_nonblocking(int ifd) {
-	int flags;
-	flags = fcntl(ifd, F_GETFL);
-	fcntl(ifd, F_SETFL, flags | O_NONBLOCK);
+  int flags;
+  flags = fcntl(ifd, F_GETFL);
+  fcntl(ifd, F_SETFL, flags | O_NONBLOCK);
 }
 
-void process_event(int epoll_fd, struct epoll_event event) {
-	char buf[1024];
-	size_t buf_size;
+void process_event(struct _server server, struct epoll_event event) {
+  char buf[BUFFER_SIZE];
+  ssize_t buf_size;
 
-	if(event.events & EPOLLIN) {
-		/* unsafe read */
-		buf_size = read(event.data.fd, buf, 1024);
-		if(buf_size == -1) {
-			/* read failed */
-			printf("read error\n");
-			exit(EXIT_FAILURE);
-		}
-		else if(buf_size == 0) {
-			printf("Nothing to read\n");
-			if(epoll_ctl(epoll_fd, EPOLL_CTL_DEL, event.data.fd, &event) == -1) {
-				exit(EXIT_FAILURE);
-			}
-			close(event.data.fd);
-		}
-		else {
-			/* unsafe write queue required */
-			if(write(event.data.fd, buf, buf_size) == -1) {
-				exit(EXIT_FAILURE);
-			}
-		}
-	}
+  if(event.events & EPOLLIN) {
+    /* unsafe read */
+    buf_size = read(event.data.fd, buf, BUFFER_SIZE);
+    if(buf_size == -1) {
+      perror("read error\n");
+      exit(EXIT_FAILURE);
+    }
+    else if(buf_size == 0) {
+      printf("Nothing to read\n");
+      if(epoll_ctl(server.epoll_socket, EPOLL_CTL_DEL, event.data.fd, &event) == -1) {
+        perror("error removing socket from epoll\n");
+        exit(EXIT_FAILURE);
+      }
+      /* very ineffecient way of searching fds */
+      darray_remove(server.clients, find_fd_index(server.clients, event.data.fd), NULL);
+      close(event.data.fd);
+    }
+    else {
+      /* unsafe write queue required */
+      if(write(event.data.fd, buf, buf_size) == -1) {
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
 }
